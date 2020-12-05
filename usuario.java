88fc4b44122f61fd09e12598a91117ed3d49b560
jlucasldm <49415623+jlucasldm@users.noreply.github.com>
2020-12-05 11:57:19 -0300

    first commit

---
+++ usuario.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Usuario{
+    private String nome;
+    private String isbn;
+    private ArrayList<Musica> listaMusicas = new ArrayList<Musica>();
+    private ArrayList<Album> listaAlbuns = new ArrayList<Album>();
+    private ArrayList<Artista> listaArtista = new ArrayList<Artista>();
+    private ArrayList<Genero> listaGeneros = new ArrayList<Genero>();
+    private ArrayList<Playlist> listaPlaylists = new ArrayList<Playlist>();
+}
